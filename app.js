98147752deea246773bd87684df754d8622092e0
Olatunji Peter <polalekan526@gmail.com>
2024-02-16 15:05:55 +0100

    Ejs added

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express")
 const app = express()
 
+app.set('view engine', 'ejs')
 app.listen(3000)
+
 app.get('/', (req,res)=>{
-    res.sendFile('./Views/index.html', {root: __dirname})
+    res.render('index', {title: "Home"})
 })
 
 app.get('/about', (req,res)=>{
-    res.sendFile('./Views/about.html', {root: __dirname})
+    res.render('about', {title: "About"})
+})
+app.get('/blog/create', (req,res)=>{
+    res.render('create', {title: "Create"})
 })
 
-app.get('/about-us', (req,res)=>{
-    res.redirect('/about')
-})
 
 app.use((req, res)=>{
-    res.status(404).sendFile('./Views/404.html', {root: __dirname})
+    res.status(404).render('404', {title: "404"})
 })
