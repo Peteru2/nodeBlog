616502689024490362a17a40143e570fdec96c16
Olatunji Peter <polalekan526@gmail.com>
2024-02-17 04:38:41 +0100

    Mongoose added

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express")
 const app = express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog'    )
+const uri = "mongodb+srv://devpeter:Mag1@cluster0.to9arvf.mongodb.net/devpeter?retryWrites=true&w=majority";
+
+mongoose.connect(uri)
+    .then((result) =>{
+        app.listen(3000)
+        console.log("Connected")
+    })
+    .catch((err) =>{
+        console.log(err)
+    })
 
 app.set('view engine', 'ejs')
-app.listen(3000)
+
+app.use(morgan('dev'));
+app.use(express.static('public'))
 
 app.get('/', (req,res)=>{
     const blogs = [
@@ -13,6 +28,21 @@ app.get('/', (req,res)=>{
     res.render('index', {title: "Home", blogs})
 })
 
+app.get('/add-blog', (req,res)=>{
+   const blog = new Blog({
+    title: 'New Blog',
+    snippet: "It is done",
+    body: "Yeah it is done",
+   })
+
+   blog.save()
+        .then((result) =>{
+            res.send(result)
+        })
+        .catch((err) =>{
+            console.log(err)
+        })
+})
 app.get('/about', (req,res)=>{
     res.render('about', {title: "About"})
 })
