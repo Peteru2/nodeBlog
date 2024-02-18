8c86a69401f3993be3e139fdc219ae2e21610964
Olatunji Peter <polalekan526@gmail.com>
2024-02-18 23:26:29 +0100

    Post sent to the database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog'    )
+const Blog = require('./models/blog')
 const uri = "mongodb+srv://devpeter:Mag1@cluster0.to9arvf.mongodb.net/devpeter?retryWrites=true&w=majority";
 
 mongoose.connect(uri)
@@ -15,9 +15,9 @@ mongoose.connect(uri)
     })
 
 app.set('view engine', 'ejs')
-
 app.use(morgan('dev'));
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req,res)=>{
     res.redirect('blogs')
@@ -51,6 +51,27 @@ app.get('/blogs', (req, res)=>{
             res.render('index', {title: "All blogs", blogs: result })
         })
     
+})
+app.post('/blogs', (req, res) =>{
+    const blog = new Blog(req.body)
+    blog.save()
+        .then((result) =>{
+            res.redirect('/blogs')
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+})
+
+app.get('/blogs/:id', (req, res) =>{
+    const id = req.params.id
+    Blog.findById(id)
+        .then((result) =>{
+            res.render('details', {title: "Blog details", blog: result })
+        })
+        .catch((err) =>{
+            console.log(err)
+        })
 })
 app.use((req, res)=>{
     res.status(404).render('404', {title: "404"})
